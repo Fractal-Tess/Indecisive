6a8d6bab47b190ce10e94f7f2053b62c5cf89728
Fractal-Tess <vgfractal@gmail.com>
2023-04-30 13:48:19 +0300

    prevent validation on build

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,18 +2,20 @@ import type { Handle } from "@sveltejs/kit";
 import { env as privEnv } from "$env/dynamic/private";
 import { env as pubEnv } from "$env/dynamic/public";
 import z from "zod";
+import { building } from "$app/environment";
 
-z.object({
-  SENDGRID_KEY: z.string(),
-  RECEIVER_EMAIL: z.string(),
-  PUBLIC_POCKETBASE_URL: z.string(),
-  PUBLIC_ORIGIN_URL: z.string().url(),
-}).parse({
-  SENDGRID_KEY: privEnv.SENDGRID_KEY,
-  RECEIVER_EMAIL: privEnv.RECEIVER_EMAIL,
-  PUBLIC_POCKETBASE_URL: pubEnv.PUBLIC_POCKETBASE_URL,
-  PUBLIC_ORIGIN_URL: pubEnv.PUBLIC_ORIGIN_URL,
-});
+if (!building)
+  z.object({
+    SENDGRID_KEY: z.string(),
+    RECEIVER_EMAIL: z.string(),
+    PUBLIC_POCKETBASE_URL: z.string(),
+    PUBLIC_ORIGIN_URL: z.string().url(),
+  }).parse({
+    SENDGRID_KEY: privEnv.SENDGRID_KEY,
+    RECEIVER_EMAIL: privEnv.RECEIVER_EMAIL,
+    PUBLIC_POCKETBASE_URL: pubEnv.PUBLIC_POCKETBASE_URL,
+    PUBLIC_ORIGIN_URL: pubEnv.PUBLIC_ORIGIN_URL,
+  });
 
 export const handle: Handle = async ({ event, resolve }) => {
   return resolve(event);
