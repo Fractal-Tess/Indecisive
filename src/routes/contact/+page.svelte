23e80d3f28ac99301abe9ca0a39ab25d9ef18894
Fractal-Tess <vgfractal@gmail.com>
2023-05-08 00:51:52 +0300

    remove w restriction

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <article class="grid place-items-center px-4 text-xl md:text-2xl">
-  <section class="sm:max-sm:max-w-xs lg:max-w-lg [&_h1]:text-3xl">
+  <section class="sm:max-sm:max-w-xs [&_h1]:text-3xl">
     {@html data.content}
   </section>
 </article>
