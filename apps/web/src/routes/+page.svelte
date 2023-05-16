651b0275d051dd4439671cb529eb166444f03e60
Fractal-Tess <vgfractal@gmail.com>
2023-05-16 03:17:59 +0300

    update workflow

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -1,51 +1,20 @@
 <script lang="ts">
-  import { afterNavigate } from '$app/navigation';
-  import { page } from '$app/stores';
   import Logo from '$lib/components/Logo.svelte';
   import { onMount } from 'svelte';
-  import TypeIt from 'typeit';
 
   let mounted = false;
-  onMount(() => {
-    mounted = true;
-  });
-  TypeIt;
-
-  const typeit = (node: HTMLParagraphElement) => {
-    let typeHead: undefined | typeof TypeIt;
-    let destroyed = false;
-
-    afterNavigate(({ from }) => {
-      if (from === null) {
-        typeHead = new TypeIt(node, {
-          afterComplete: () => {
-            if (!destroyed) {
-              typeHead.destroy();
-              destroyed = true;
-            }
-          },
-          speed: 80
-        }).go();
-      }
-    });
-    return {
-      destroy() {
-        if (!destroyed && typeHead) {
-          typeHead.destroy();
-          destroyed = true;
-        }
-      }
-    };
-  };
+  onMount(() => (mounted = true));
 </script>
 
 <section
-  class="flex select-none flex-col items-center justify-center gap-y-8 text-center">
-  <p use:typeit class="text-3xl md:text-4xl [&_em]:text-yellow-400">
-    Welcome to <em><strong>Indecisive</strong></em>
+  class="flex select-none flex-col items-center justify-center gap-y-8 text-center
+  [&>*]:transition-opacity [&>*]:duration-500
+  ">
+  <p
+    class={`text-3xl delay-1000 duration-1000 md:text-4xl
+   ${mounted ? 'opacity-100' : 'opacity-0'} `}>
+    Welcome to <span class="text-primary font-bold italic"> Indecisive</span>
   </p>
   <Logo
-    class={`${
-      mounted ? 'opacity-100' : 'opacity-0'
-    } h-60 transition-opacity duration-[3000]`} />
+    class={`${mounted ? 'opacity-100' : 'opacity-0'} h-60 md:h-80 lg:h-96  `} />
 </section>
