94b2c5e32515e918e6aae36e8aec85a0bf0d691d
Fractal-Tess <vgfractal@gmail.com>
2023-05-19 08:37:48 +0300

    handle empty images and do not send empty form field to db

--- apps/web/src/lib/pocketbase/utils.ts
+++ apps/web/src/lib/pocketbase/utils.ts
@@ -7,8 +7,17 @@ export const saveApplicationToDatabase = async (record: Application) => {
   const formData = new FormData();
 
   for (const [key, val] of Object.entries(record)) {
-    const v = val instanceof Number ? val.toString() : (val as string | Blob);
-    formData.append(key, v);
+    if (val instanceof Blob && val.size > 0) {
+      formData.append(key, val);
+    } else if (typeof val === 'number') {
+      formData.append(key, val.toString());
+    } else if (typeof val === 'string') {
+      formData.append(key, val);
+    } else {
+      console.log(
+        `Should be unreachable - app db parser  key ${key} is instance of ${typeof val}`
+      );
+    }
   }
 
   const result = await pb.collection('application').create(formData);
