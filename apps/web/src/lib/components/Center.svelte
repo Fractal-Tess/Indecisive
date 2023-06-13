6ee3d94232ae8c84ae749829a0a83be72ae0ad05
Fractal-Tess <vgfractal@gmail.com>
2023-06-14 00:00:07 +0300

    new components

---
+++ apps/web/src/lib/components/Center.svelte
@@ -0,0 +1,3 @@
+<div class="flex min-h-full items-center justify-center">
+  <slot />
+</div>
---
+++ apps/web/src/lib/components/Spinner.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { fade } from 'svelte/transition';
+
+  export let ANIMATION_SPEED = 250;
+</script>
+
+<div
+  transition:fade={{ duration: ANIMATION_SPEED }}
+  class="form-control items-center justify-center [grid-area:stack]">
+  <span class="loading loading-infinity text-primary w-24" />
+</div>
