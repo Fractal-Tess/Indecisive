c2f34e2eca9c8219ae47b4200789f29d1bf6ecd0
Fractal-Tess <vgfractal@gmail.com>
2023-06-13 23:57:33 +0300

    added user and pb to locals

--- apps/web/src/app.d.ts
+++ apps/web/src/app.d.ts
@@ -1,8 +1,8 @@
-// See https://kit.svelte.dev/docs/types#app
-// for information about these interfaces
-// and what to do when importing types
 declare namespace App {
-  // interface Locals {}
+  interface Locals {
+    pb: import('pocketbase').default;
+    user: import('$lib/stores/user').User | null;
+  }
   // interface PageData {}
   // interface Error {}
   // interface Platform {}
