c54a4d4499845471c79810caeb97a7a195e0684b
Fractal-Tess <vgfractal@gmail.com>
2023-06-14 00:01:11 +0300

    renamed import name and added comment

--- apps/web/src/lib/pocketbase/pocketbase.ts
+++ apps/web/src/lib/pocketbase/pocketbase.ts
@@ -1,4 +1,7 @@
-import Pocketbase from 'pocketbase';
 import { PUBLIC_POCKETBASE_URL } from '$env/static/public';
+import PocketBase from 'pocketbase';
 
-export const pb = new Pocketbase(PUBLIC_POCKETBASE_URL);
+export const pb = new PocketBase(PUBLIC_POCKETBASE_URL);
+
+// When on the server, user the `pb` instance on the locals
+// DO NOT USE THIS PB INSTANCE ON THE SERVER SINCE IT IS SHARED.
